1c75b7618d6bc8566c60cc36582585f5f50a5b68
WangShengyu <wangshengyu@51hitech.com>
2018-07-11 10:55:27 +0800

    Update get_package_name.py.

--- get_package_name.py
+++ get_package_name.py
@@ -1,7 +1,8 @@
 import subprocess
 import re
+import os
 
-project_name = "commonutils"
+project_name = "posefilter"
 user_name = "cybertroneye"
 
 def get_output(cmd):
@@ -11,21 +12,25 @@ def get_output(cmd):
 def extract_version(raw_version):
   m = re.search('VERSION_([0-9]*)_([0-9]*).*', raw_version)
   if m:
-    return m.group(1) + "." + m.group(2)
+    return m.group(1), m.group(2)
   else:
     print "Generate version fail. Can't find a tag like VERSION_X_X"
     return None
 
 raw_version = get_output("git describe --match VERSION_[0-9]*_[0-9]*")
-version = extract_version(raw_version)
-if version == None:
+version0, version1 = extract_version(raw_version)
+if version0 == None:
   exit(1)
-subversion = get_output("git rev-list --count HEAD")
+version = version0 + "." + version1
+version_tag_commit = get_output("git rev-list -n 1 VERSION_" + version0 + "_" + version1)
+subversion = get_output("git rev-list --count " + version_tag_commit)
 commit = get_output("git log --format=%h -n 1")
 
-branch = get_output("git branch")
-if branch.startswith("* "):
-  branch = branch[2:]
+branch_list = get_output("git branch").split("\n")
+cur_branch = ""
+for branch in branch_list:
+  if branch.startswith("* "):
+    cur_branch = branch[2:]
 
 total_version = version + "." + subversion + "." + commit
-print project_name + "/" + total_version + "@" + user_name + "/" + branch
+print project_name + "/" + total_version + "@" + user_name + "/" + cur_branch
